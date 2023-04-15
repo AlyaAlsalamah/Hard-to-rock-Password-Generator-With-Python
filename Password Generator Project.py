960fa0cadebfe876f1df99e2a31c6d4b24bb4a82
Alya  Alsalamah <109030320+AlyaAlsalamah@users.noreply.github.com>
2023-04-15 07:26:25 +0300

    Add files via upload

---
+++ Password Generator Project.py
@@ -0,0 +1,30 @@
+#Password Generator Project
+import random
+import string
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+letters_in_password = ''.join((random.choice(letters) for i in range(nr_letters)))
+numbers_in_password = ''.join((random.choice(numbers) for i in range(nr_numbers)))
+symbols_in_password = ''.join((random.choice(symbols) for i in range(nr_symbols)))
+#print(letters_in_password + numbers_in_password + symbols_in_password)
+Not_orderd_pass1=[]
+Not_orderd_pass1+= letters_in_password
+Not_orderd_pass1+= numbers_in_password 
+Not_orderd_pass1+= symbols_in_password
+random.shuffle(Not_orderd_pass1)
+password=''
+for i in Not_orderd_pass1:
+  password+= i 
+  
+print(password)
